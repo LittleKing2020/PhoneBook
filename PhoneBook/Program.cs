5bbdb8910c96d727a34b4ce9045bf1997136c92d
Alireza <Alireza@ALIREZA-PC>
2020-06-08 15:41:50 +0430

    Add project files.

---
+++ PhoneBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To PhoneBook");
+            Console.WriteLine("*--------------------------------------*");
+            Console.WriteLine("");
+            Console.WriteLine("Add a Contact");
+            Console.WriteLine("Delete a Contact");
+            Console.WriteLine("Update a Contatct");
+            Console.WriteLine("Search by Name");
+            Console.WriteLine("Search by Number");
+            Console.WriteLine("");
+            Console.Write("Please Select Option");
+            string option = Console.ReadLine();
+        }
+    }
+}
